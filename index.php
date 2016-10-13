e9d956bf3b7e055d372844f0eb94dada1f8f3e44
周蓝萍 <hzzhoulanping@corp.netease.com>
2016-10-13 15:52:35 +0800

    第一版提交

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<?php
+require_once __DIR__ . '/autoload.php';
+
+
+use VideoCloud\Storage\UploadManager;
+
+
+$uploadMgr = new UploadManager();
+
+
+$opt=array();
+$opt["accessKey"]="your AppKey";
+$opt["secretKey"]="your AppSecret";
+$opt["trunkSize"]= 2 * 1024 * 1024;
+
+$filePath = './php-logo.png';
+
+list($ret, $err) = $uploadMgr->init($opt, $filePath);
+if ($err !== null) {
+    die($err);
+}
+
+list($ret, $err) = $uploadMgr->upload($ret['ret'],$filePath);
+if ($err !== null) {
+    var_dump($err);
+} else {
+    var_dump($ret);
+}
